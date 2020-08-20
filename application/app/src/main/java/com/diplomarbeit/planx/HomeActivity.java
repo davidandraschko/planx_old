1003efb6c7a1de2deb523f3b3b8931c066f26145
davidandraschko <d.andraschko@students.htl-leonding.ac.at>
2020-08-20 15:08:51 +0200

    Implemented gesture swipe between activities

--- application/app/src/main/java/com/diplomarbeit/planx/HomeActivity.java
+++ application/app/src/main/java/com/diplomarbeit/planx/HomeActivity.java
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.MotionEvent;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
@@ -11,6 +12,8 @@ import android.widget.ImageView;
 import maes.tech.intentanim.CustomIntent;
 
 public class HomeActivity extends AppCompatActivity implements View.OnClickListener{
+
+    float x1, x2, y1, y2;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,6 +28,30 @@ public class HomeActivity extends AppCompatActivity implements View.OnClickListe
 
         ImageView imageViewPost = findViewById(R.id.imageViewPost);
         imageViewPost.setOnClickListener(this);
+    }
+
+    public boolean onTouchEvent(MotionEvent touchevent){
+        switch (touchevent.getAction()){
+            case MotionEvent.ACTION_DOWN:
+                x1 = touchevent.getX();
+                y1 = touchevent.getY();
+                break;
+             case MotionEvent.ACTION_UP:
+                x2 = touchevent.getX();
+                y2 = touchevent.getY();
+                if(x1 < x2){
+                    Intent intent = new Intent(this, CalendarActivity.class);
+                    startActivity(intent);
+                    CustomIntent.customType(this, "right-to-left");
+                }
+                 if(x1 > x2){
+                     Intent intent = new Intent(this, PostActivity.class);
+                     startActivity(intent);
+                     CustomIntent.customType(this, "left-to-right");
+                 }
+                break;
+        }
+        return false;
     }
 
     @Override
--- application/app/src/main/java/com/diplomarbeit/planx/PostActivity.java
+++ application/app/src/main/java/com/diplomarbeit/planx/PostActivity.java
@@ -2,16 +2,39 @@ package com.diplomarbeit.planx;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.MotionEvent;
 
 import maes.tech.intentanim.CustomIntent;
 
 public class PostActivity extends AppCompatActivity {
 
+    float x1, x2, y1, y2;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_post);
+    }
+
+    public boolean onTouchEvent(MotionEvent touchevent){
+        switch (touchevent.getAction()){
+            case MotionEvent.ACTION_DOWN:
+                x1 = touchevent.getX();
+                y1 = touchevent.getY();
+                break;
+            case MotionEvent.ACTION_UP:
+                x2 = touchevent.getX();
+                y2 = touchevent.getY();
+                if(x1 < x2){
+                    Intent intent = new Intent(this, HomeActivity.class);
+                    startActivity(intent);
+                    CustomIntent.customType(this, "right-to-left");
+                }
+                break;
+        }
+        return false;
     }
 
     @Override
