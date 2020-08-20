acb9af99bd64f6d9a32ecd38a419007d0e815da1
davidandraschko <d.andraschko@students.htl-leonding.ac.at>
2020-08-20 13:33:39 +0200

    Created other activities

---
+++ application/app/src/main/java/com/diplomarbeit/planx/PostActivity.java
@@ -0,0 +1,14 @@
+package com.diplomarbeit.planx;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class PostActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_post);
+    }
+}
