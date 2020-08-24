9710612d0bffe877edb82bf54b26f921c488679a
davidandraschko <d.andraschko@students.htl-leonding.ac.at>
2020-08-24 15:25:14 +0200

    Implemented login with Instagram via Facebook

--- application/app/src/main/java/com/diplomarbeit/planx/HomeActivity.java
+++ application/app/src/main/java/com/diplomarbeit/planx/HomeActivity.java
@@ -3,17 +3,38 @@ package com.diplomarbeit.planx;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.os.AsyncTask;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.MotionEvent;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.bumptech.glide.Glide;
+import com.facebook.AccessToken;
+import com.facebook.GraphRequest;
+import com.facebook.GraphResponse;
+
+import org.json.JSONException;
+
+import java.io.InputStream;
+import java.net.HttpURLConnection;
+import java.net.URL;
 
 import maes.tech.intentanim.CustomIntent;
 
 public class HomeActivity extends AppCompatActivity implements View.OnClickListener{
 
     float x1, x2, y1, y2;
+
+    private ImageView imageViewPb;
+    String profilePictureUrl;
+
+    TextView textView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -28,6 +49,55 @@ public class HomeActivity extends AppCompatActivity implements View.OnClickListe
 
         ImageView imageViewPost = findViewById(R.id.imageViewPost);
         imageViewPost.setOnClickListener(this);
+
+        imageViewPb = findViewById(R.id.imageViewPb);
+        getInstagramData(AccessToken.getCurrentAccessToken());
+
+        textView = findViewById(R.id.textView);
+
+    }
+
+    private void getInstagramData(AccessToken accessToken) {
+        final GraphRequest request = GraphRequest.newGraphPathRequest(accessToken,
+                "17841438891721670?fields=biography,followers_count,media_count,username,profile_picture_url,follows_count,name", new GraphRequest.Callback() {
+                    @Override
+                    public void onCompleted(GraphResponse response) {
+                        Log.i("RESPONSE", response.toString());
+                        try {
+                            profilePictureUrl = response.getJSONObject().getString("profile_picture_url");
+                            textView.setText("Hello, " + response.getJSONObject().getString( "username"));
+                            Log.i("PROFILBILD", profilePictureUrl);
+                            ProfilePictureDownloader pictureDownloader = new ProfilePictureDownloader();
+                            Bitmap profilePicture;
+                            try {
+                                profilePicture = pictureDownloader.execute(profilePictureUrl).get();
+                                imageViewPb.setImageBitmap(profilePicture);
+                            } catch (Exception e) {
+                                e.printStackTrace();
+                            }
+                        } catch (JSONException e) {
+                            e.printStackTrace();
+                        }
+                    }
+                });
+        request.executeAsync();
+    }
+
+    public class ProfilePictureDownloader extends AsyncTask<String, Void, Bitmap> {
+        @Override
+        protected Bitmap doInBackground(String... urls) {
+            try {
+                URL url = new URL(urls[0]);
+                HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+                connection.connect();
+                InputStream inputStream = connection.getInputStream();
+                Bitmap myBitmap = BitmapFactory.decodeStream(inputStream);
+                return myBitmap;
+            } catch (Exception e) {
+                e.printStackTrace();
+                return null;
+            }
+        }
     }
 
     public boolean onTouchEvent(MotionEvent touchevent){
@@ -73,4 +143,7 @@ public class HomeActivity extends AppCompatActivity implements View.OnClickListe
             CustomIntent.customType(this, "left-to-right");
         }
     }
+
+
+
 }
--- application/app/src/main/java/com/diplomarbeit/planx/MainActivity.java
+++ application/app/src/main/java/com/diplomarbeit/planx/MainActivity.java
@@ -1,30 +1,75 @@
 package com.diplomarbeit.planx;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity implements View.OnClickListener{
+import com.facebook.AccessToken;
+import com.facebook.AccessTokenTracker;
+import com.facebook.CallbackManager;
+import com.facebook.FacebookCallback;
+import com.facebook.FacebookException;
+import com.facebook.login.LoginManager;
+import com.facebook.login.LoginResult;
+import com.facebook.login.widget.LoginButton;
+
+import java.util.Arrays;
+
+public class MainActivity extends AppCompatActivity {
+
+    LoginButton buttonLoginFb;
+    private CallbackManager callbackManager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        Button buttonLogin = findViewById(R.id.buttonLogin);
-        Button buttonLoginFb = findViewById(R.id.buttonLoginFb);
+        buttonLoginFb = findViewById(R.id.buttonLoginFb);
 
-        buttonLogin.setOnClickListener(this);
+        callbackManager = CallbackManager.Factory.create();
+        LoginManager.getInstance()
+                .logInWithReadPermissions(this,
+                        Arrays.asList("email, instagram_basic, pages_read_engagement, pages_show_list, " +
+                                "user_birthday, ads_management, business_management, instagram_manage_insights," +
+                                "pages_manage_posts"));
+
+        buttonLoginFb.registerCallback(callbackManager, new FacebookCallback<LoginResult>() {
+            @Override
+            public void onSuccess(LoginResult loginResult) {
+                Log.i("BERECHTIGUNGEN", AccessToken.getCurrentAccessToken().getPermissions().toString());
+                Log.i("ABGELEHNT", AccessToken.getCurrentAccessToken().getDeclinedPermissions().toString());
+                Log.i("ACCESS_TOKEN", AccessToken.getCurrentAccessToken().getToken());
+                Intent intent = new Intent(MainActivity.this, HomeActivity.class);
+                startActivity(intent);
+            }
+
+            @Override
+            public void onCancel() {
+
+            }
+
+            @Override
+            public void onError(FacebookException error) {
+                Log.i("ERR-BERECHTIGUNGEN", AccessToken.getCurrentAccessToken().getPermissions().toString());
+                Log.i("ERR-ABGELEHNT", AccessToken.getCurrentAccessToken().getDeclinedPermissions().toString());
+            }
+        });
+
     }
 
     @Override
-    public void onClick(View view) {
-        if (view.getId() == R.id.buttonLogin) {
-            Intent intent = new Intent(this, HomeActivity.class);
-            this.startActivity(intent);
-        }
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        callbackManager.onActivityResult(requestCode, resultCode, data);
+        super.onActivityResult(requestCode, resultCode, data);
     }
 }
