d6ff030230237b6349085dd6dda4688ce08dd714
davidandraschko <d.andraschko@students.htl-leonding.ac.at>
2020-08-26 16:13:03 +0200

    Added spinner dropdown, DatePicker, timePicker

--- application/app/src/main/java/com/diplomarbeit/planx/PostActivity.java
+++ application/app/src/main/java/com/diplomarbeit/planx/PostActivity.java
@@ -1,10 +1,33 @@
 package com.diplomarbeit.planx;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.DatePickerDialog;
+import android.app.TimePickerDialog;
+import android.content.Context;
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
+import android.net.Uri;
+import android.nfc.Tag;
 import android.os.Bundle;
+import android.provider.MediaStore;
+import android.util.Log;
 import android.view.MotionEvent;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.DatePicker;
+import android.widget.ImageView;
+import android.widget.Spinner;
+import android.widget.TextView;
+import android.widget.TimePicker;
+
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.sql.Time;
+import java.util.Calendar;
 
 import maes.tech.intentanim.CustomIntent;
 
@@ -12,11 +35,119 @@ public class PostActivity extends AppCompatActivity {
 
     float x1, x2, y1, y2;
 
+    private TextView textViewDate;
+    private TextView textViewTime;
+    private DatePickerDialog.OnDateSetListener dateSetListener;
+    private TimePickerDialog.OnTimeSetListener timeSetListener;
+
+    private ImageView imageViewPosting;
+    //int SELECT_PHOTO = 1;
+    //Uri uri;
+
+    Context context = this;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_post);
+
+        Spinner spinner = findViewById(R.id.hashtagSpinner);
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this, R.array.hashtag_presets, android.R.layout.simple_spinner_item);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinner.setAdapter(adapter);
+
+        imageViewPosting = findViewById(R.id.imageViewPosting);
+        /*imageViewPosting.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick (View view) {
+                Intent intent = new Intent(Intent.ACTION_PICK);
+                intent.setType("image/");
+                startActivityForResult(intent, SELECT_PHOTO);
+            }
+        });*/
+
+
+        textViewDate = findViewById(R.id.textViewDate);
+        textViewTime = findViewById(R.id.textViewTime);
+
+        textViewDate.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view){
+                Calendar cal = Calendar.getInstance();
+                int year = cal.get(Calendar.YEAR);
+                int month = cal.get(Calendar.MONTH);
+                int day = cal.get(Calendar.DAY_OF_MONTH);
+
+                DatePickerDialog dialog = new DatePickerDialog(
+                        PostActivity.this,
+                        android.R.style.Theme_Holo_Light_Dialog_MinWidth,
+                        dateSetListener,
+                        year, month,day);
+                dialog.getWindow().setBackgroundDrawable(new ColorDrawable(Color.TRANSPARENT));
+                dialog.show();
+            }
+        });
+
+        dateSetListener = new DatePickerDialog.OnDateSetListener() {
+            @Override
+            public void onDateSet(DatePicker datePicker, int year, int month, int day) {
+                String date = day + "/" + month + "/" + year;
+                textViewDate.setText(date);
+            }
+        };
+
+
+        textViewTime.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view){
+                Calendar cal = Calendar.getInstance();
+                int hour = cal.get(Calendar.HOUR_OF_DAY);
+                int minute = cal.get(Calendar.MINUTE);
+
+                TimePickerDialog dialog = new TimePickerDialog(
+                        PostActivity.this,
+                        android.R.style.Theme_Holo_Light_Dialog_MinWidth,
+                        timeSetListener,
+                        hour, minute, android.text.format.DateFormat.is24HourFormat(context));
+                dialog.getWindow().setBackgroundDrawable(new ColorDrawable(Color.TRANSPARENT));
+                dialog.show();
+            }
+        });
+
+        dateSetListener = new DatePickerDialog.OnDateSetListener() {
+            @Override
+            public void onDateSet(DatePicker datePicker, int year, int month, int day) {
+                String date = day + "/" + month + "/" + year;
+                textViewDate.setText(date);
+            }
+        };
+
+        timeSetListener = new TimePickerDialog.OnTimeSetListener() {
+            @Override
+            public void onTimeSet(TimePicker timePicker, int hour, int minute) {
+                String time = hour + ":" + minute;
+                textViewTime.setText(time);
+            }
+        };
     }
+
+   /* @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (resultCode == SELECT_PHOTO && resultCode == RESULT_OK && data != null && data.getData() != null){
+            uri = data.getData();
+
+            try{
+                Bitmap bitmap = MediaStore.Images.Media.getBitmap(getContentResolver(), uri);
+                imageViewPosting.setImageBitmap(bitmap);
+                Log.i("Test", "Test");
+            } catch (FileNotFoundException e) {
+                e.printStackTrace();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }*/
 
     public boolean onTouchEvent(MotionEvent touchevent){
         switch (touchevent.getAction()){
