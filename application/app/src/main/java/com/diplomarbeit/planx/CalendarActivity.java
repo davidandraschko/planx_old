a574e27428457f711aeb1bef8345d06e8fcb64ea
davidandraschko <d.andraschko@students.htl-leonding.ac.at>
2020-09-03 14:06:42 +0200

    Implemented code to create a post

--- application/app/src/main/java/com/diplomarbeit/planx/CalendarActivity.java
+++ application/app/src/main/java/com/diplomarbeit/planx/CalendarActivity.java
@@ -4,7 +4,19 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.provider.CalendarContract;
+import android.util.Log;
 import android.view.MotionEvent;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.CalendarView;
+import android.widget.ListView;
+
+import com.google.gson.Gson;
+
+import java.util.ArrayList;
+import java.util.Calendar;
 
 import maes.tech.intentanim.CustomIntent;
 
@@ -12,10 +24,34 @@ public class CalendarActivity extends AppCompatActivity {
 
     float x1, x2, y1, y2;
 
+    CalendarView calendar;
+
+    ListView listView;
+
+    ArrayList events;
+
+    PostActivity postActivity;
+
+    Button buttonGetPost;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_calendar);
+
+        //calendar = findViewById(R.id.calendarView);
+        //listView = findViewById(R.id.listView);
+        //events = new ArrayList<>();
+
+        /*events.add("Test1");
+        events.add("Test2");
+        events.add("Test3");
+        events.add(12);*/
+
+        ArrayAdapter<String> arrayAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, events);
+
+        listView.setAdapter(arrayAdapter);
+
     }
 
     public boolean onTouchEvent(MotionEvent touchevent){
